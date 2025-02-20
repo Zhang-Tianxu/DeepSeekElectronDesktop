ca96f7e20b843ba703ea3b9df4efaac5b09d8d9a
tianxu <aoxiaotian@foxmail.com>
2025-02-20 21:41:35 +0800

    index.css重命名为main.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { Provider } from 'react-redux'
-import './index.css'
+import './main.css'
 import App from './App.jsx'
 // import { store } from './test_store'
 
