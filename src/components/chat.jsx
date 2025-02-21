0ae47d217b8b7531dadd728be9f1a9c05525c319
tianxu <aoxiaotian@foxmail.com>
2025-02-21 13:32:12 +0800

    调整UI布局

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,73 +1,61 @@
 
 import { useState } from 'react'
-import React, { useEffect } from 'react'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default function DeepSeekChat() {
-  const [count, setCount] = useState(0)
-  const [chatHistory, setChatHistory] = useState([])
-  const [userMsg, setUserMsg] = useState('1+1等于几？')
-  const [answer, setAnwer] = useState('')
+    const [chatHistory, setChatHistory] = useState([])
+    const [userMsg, setUserMsg] = useState('1+1等于几？')
 
-    /*
-    useEffect(() =>{
-        (
-            async () => {
-                const response = await fetch('http://127.0.0.1:8000/stream/在Electron中，如何让用户点击按钮调用main线程的function？');
-                const reader = response.body.getReader();
-                const decoder = new TextDecoder();
-                const { done, value } = await reader.read();
-                console.log(decoder.decode(value))
-                setResponse(response + decoder.decode(value))
-                if (done) {
-                    console.log(done)
-                };
-            }
-        )();
+    return (
+        <div style={{ height: "100%", alignItems: "center", justifyContent: "flex-end", display: "flex", flexDirection: "column", marginBottom: "2vh" }}>
+            <div style={{ overflowY: "auto", scrollbarWidth: "none", height: "90%" }}>
+                {chatHistory.map((item, idx) => (
+                    <div key={idx} style={{ width: "100%", margin: '10px', justifySelf: item['role'] == 'user' ? 'right' : 'left', textAlign: item['role'] == 'user' ? 'right' : 'left', display: "flex", justifyContent: "flex-end" }}>
+                        <div style={{ width: "fit-content", border: "1px solid", borderRadius: "10px", padding: "5px" }}>
+                            <ReactMarkdown>
+                                {item['content']}
+                            </ReactMarkdown>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div style={{ width: "100%", height: "10%", padding: "1rem 0 1rem 0", justifyContent: "space-around", alignItems: "center", display: "flex" }}>
+                <textarea
+                    style={{ width: "80%", height: "5rem", borderRadius: "20px", padding: "10px" }}
+                    value={userMsg} onInput={(e) => {
+                        setUserMsg(e.target.value)
+                    }}></textarea>
+                <button
+                    style={{ padding: "0.5rem 1rem 0.5rem 1rem", backgroundColor: "green", color: "white" }}
+                    onClick={async () => {
+                        setChatHistory([...chatHistory, { 'role': 'user', 'content': userMsg }])
+                        const gpu = await fetch('http://localhost:50055/gpu_available/');
+                        const gpu_res = await gpu.json()
 
-    }, [response])
-    */
+                        const deepSeekHost = await window.globalVariables.getDeepSeekBackendHost()
+                        //   const response = await fetch('http://localhost:8000/stream/' + userMsg);
+                        //   const response = await fetch('http://localhost:8002/stream/' + userMsg);
+                        const response = await fetch(deepSeekHost + '/stream/' + userMsg);
+                        const reader = response.body.getReader();
+                        const decoder = new TextDecoder();
+                        let newChat = { 'role': 'assistant', 'content': '' }
 
-
-//   let chat_his = [
-// ]
-
-  return (
-    <>
-    {chatHistory.map((item, idx) => (
-        <div key={idx} style={{alignContent: item['role'] == 'user' ? 'left' : 'right', textAlign:item['role'] == 'user' ? 'left': 'right', backgroundColor: item['role'] == 'user' ? 'red' : 'green', margin:'10px'}}>
-            <div>{item['role']} : {item['content']}</div>
+                        while (true) {
+                            await new Promise(r => setTimeout(r, 100))
+                            const { done, value } = await reader.read();
+                            if (done) {
+                                console.log("done")
+                                setChatHistory([...chatHistory, { 'role': 'user', 'content': userMsg }, newChat])
+                                break;
+                            } else {
+                                console.log(decoder.decode(value))
+                                newChat['content'] = newChat['content'] + decoder.decode(value)
+                                setChatHistory([...chatHistory, { 'role': 'user', 'content': userMsg }, newChat])
+                            }
+                        }
+                    }}>发送</button>
+            </div>
         </div>
-    ))}
-    <div>
-    {answer}
-    </div>
-    <textarea value={userMsg} onInput={(e)=>{
-        setUserMsg(e.target.value)
-    }}></textarea>
-    <button onClick={async ()=>{
-              setChatHistory([...chatHistory, {'role':'user', 'content':userMsg}])
-            //   const response = await fetch('http://localhost:8000/stream/' + userMsg);
-              const response = await fetch('http://localhost:8002/stream/' + userMsg);
-              const reader = response.body.getReader();
-              const decoder = new TextDecoder();
-            //   let LLMAnswer = ""
-              let newChat = {'role':'assistant', 'content': ''}
-
-              while (true) {
-                  await new Promise(r => setTimeout(r, 100))
-                  const { done, value } = await reader.read();
-                  if (done) 
-                  {
-                      console.log("done")
-                      setChatHistory([...chatHistory, {'role':'user', 'content':userMsg},newChat])
-                    break;
-                  } else {
-                    console.log(decoder.decode(value))
-                    newChat['content'] = newChat['content'] + decoder.decode(value)
-                    setChatHistory([...chatHistory,{'role':'user', 'content':userMsg} ,newChat])
-                  }
-              }
-    }}>send</button>
-    </>
-  )
+    )
 }
