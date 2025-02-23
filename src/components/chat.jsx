870f3c39a8de0e3df531c44044caa1d274a8cf7c
tianxu <aoxiaotian@foxmail.com>
2025-02-24 07:46:17 +0800

    调整Chat页面UI布局
    包括切分<think> tag

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -2,11 +2,37 @@
 import { useState } from 'react'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import { Spin } from 'antd'
+import { Spin, Button } from 'antd'
+import {UpCircleOutlined} from '@ant-design/icons';
+function spliteAIAnswer(str) {
+    console.log(str)
+
+    const regx1 = /((.|\n|\t|\r)*)(<think>((.|\n|\t|\r)*)<\/think>)((.|\n|\t|\r)*)/;
+
+    const regx2 = /((.|\n|\t|\r)*)(<think>((.|\n|\t|\r)*)(<\/think>)?)((.|\n|\t|\r)*)/;
+
+    // Regular expression to match the three parts, with the closing </think> tag optional
+
+    // Apply regex to the string
+    const match1 = str.match(regx1); // 123<think>456</think>789
+    const match2 = str.match(regx2); // 123<think>456
+    if(match1) {
+        // question<think>think</think>answer
+        return [str, match1[1].trim(), match1[4].trim(),match1[6].trim()]
+    }
+    if(match2) {
+        // question<think>think
+        return [str,match2[1].trim(), match2[4].trim(), ""]
+    }
+    // question
+    return [str, str, "", ""]
+
+}
 
 export default function DeepSeekChat() {
     const [chatHistory, setChatHistory] = useState([])
-    const [userMsg, setUserMsg] = useState('1+1等于几？')
+    const [userMsg, setUserMsg] = useState('')
+
 
     return (
         <div style={{ height: "100%", alignItems: "center", justifyContent: "flex-end", display: "flex", flexDirection: "column", marginBottom: "2vh" }}>
@@ -22,10 +48,18 @@ export default function DeepSeekChat() {
                         color: item['role'] == 'user' ? "white" : "black",
                         borderRadius: "10px", padding: "5px" }}>
                             { !item['finish'] &&
-                                <Spin></Spin>
+                                <Spin style={{paddingBottom:"1rem"}}></Spin>
                             }
-                            <ReactMarkdown style={{width:"80%", margin:"0"}}>
-                                {item['content']}
+                            {item['role'] == 'user' && <div>{spliteAIAnswer(item['content'])[0]}</div>}
+                            {item['role'] != 'user' && <div style={{backgroundColor:"gray", border:"none", borderRadius:"20px", padding:"1rem", color:"white"}}>
+                                <div>
+                                    <b>思考：</b>
+                                </div>
+                                {spliteAIAnswer(item['content'])[2]}
+                            </div>}
+                            <ReactMarkdown className="react-markdown">
+                                {/* newChat['content'] = spliteAIAnswer(newChat['content']) */}
+                            {spliteAIAnswer(item['content'])[3]}
                             </ReactMarkdown>
                         </div>
                     </div>
@@ -37,9 +71,13 @@ export default function DeepSeekChat() {
                     value={userMsg} onInput={(e) => {
                         setUserMsg(e.target.value)
                     }}></textarea>
-                <button
-                    style={{ padding: "0.5rem 1rem 0.5rem 1rem", backgroundColor: "green", color: "white" }}
+                <Button
+                type='primary'
+                disabled={userMsg.length == 0}
+                icon={<UpCircleOutlined/>}
+                    // style={{ padding: "0.5rem 1rem 0.5rem 1rem", backgroundColor: "green", color: "white" }}
                     onClick={async () => {
+                        setUserMsg("")
                         setChatHistory([...chatHistory, { 'role': 'user', 'content': userMsg, 'finish':true }])
 
                         const deepSeekHost = await window.globalVariables.getDeepSeekBackendHost()
@@ -69,7 +107,7 @@ export default function DeepSeekChat() {
                                 setChatHistory([...chatHistory, { 'role': 'user', 'content': userMsg,'finish':true }, newChat])
                             }
                         }
-                    }}>发送</button>
+                    }}>发送</Button>
             </div>
         </div>
     )
